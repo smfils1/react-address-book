0ce8d504536d33f9fa9630e5cccbfb52d3b08406
Samuel Fils <smfils1@gmail.com>
2019-10-04 12:10:56 -0400

    Added contactList & initial data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import AddContactPage from '../AddContactPage/AddContactPage';
 import ContactsPage from '../ContactsPage/ContactsPage'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from '../NavBar/NavBar'
+import ContactList from '../ContactList/ContactList'
 
 function App() {
   return (
@@ -15,7 +16,44 @@ function App() {
         <Route path="/add-contact" render={AddContactPage}/>    
         <Route render={ContactsPage}/>    
       </Switch>
-
+      <ContactList contacts={[
+    {
+        FirstName: "Cathy" ,
+        LastName: "Pierce",
+        Birthday: "9/14/1996",
+        Telephone: "200-910-8132"
+    },
+    {
+        FirstName: "Alfonso",
+        LastName: "Cooley",
+        Birthday: "8/10/1973",
+        Telephone: "200-657-9362"
+    },
+    {
+        FirstName: "Victor",
+        LastName: "Gordon",
+        Birthday:  "8/3/1970",
+        Telephone: "200-661-9407"
+    },
+    {
+        FirstName: "Colleen",
+        LastName: "Wright",
+        Birthday: "10/28/1967",
+        Telephone: "200-250-7949"
+    },
+    {
+        FirstName: "James",
+        LastName: "Johnston",
+        Birthday: "5/11/1972",
+        Telephone: "200-645-3176"
+    },
+    {
+        FirstName: "Anna",
+        LastName: "Reyes",
+        Birthday: "9/10/1975",
+        Telephone: "200-707-8670"
+    }
+]}/>
     </Router>
 </>
   );
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,22 +6,22 @@ import Button from "react-bootstrap/Button";
 import defaultImg from "../../assets/default-img.png";
 import { FaRegTrashAlt } from "react-icons/fa";
 
-const Contact = ({ firstName, lastName, birthday, telephone }) => (
-    <Card style={{ maxWidth: "25rem" }}>
+const Contact = ({ FirstName, LastName, Birthday, Telephone, width} ) => (
+    <Card style={{ width}}>
         <Row className="no-gutters">
-            <Col xs={4}>
+            <Col xs={3}>
                 <Card.Img src={defaultImg} />
             </Col>
-            <Col xs={6}>
-                <Card.Body>
-                    <Card.Title>{`${firstName}  ${lastName}`}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">
-                        {telephone}
+            <Col xs={7}>
+                <div className="ml-3 mt-2">
+                    <Card.Title>{`${FirstName}  ${LastName}`}</Card.Title>
+                    <Card.Subtitle className="mb-1 text-muted">
+                        {Telephone}
                     </Card.Subtitle>
-                    <Card.Text>{birthday}</Card.Text>
-                </Card.Body>
+                    <Card.Text>{Birthday}</Card.Text>
+                </div>
             </Col>
-            <Col xs={2} className="mt-auto mb-2">
+            <Col xs={2} className="mt-auto mb-2 text-center">
                 <Button variant="danger">
                     <FaRegTrashAlt />
                 </Button>
