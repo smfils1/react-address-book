1689a9ded41f95dfe0e0fec81f045e31b9fb921f
Samuel Fils <smfils1@gmail.com>
2019-10-05 16:33:49 -0400

    Updates

---
+++ precache-manifest.61be501325e471ca072bf40dbe89b6b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff349b07f2e94e26f2b260334db84f4",
+    "url": "/react_address_book/index.html"
+  },
+  {
+    "revision": "f91742afd884d4fa4640",
+    "url": "/react_address_book/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "32c97ffce2506f6000c6",
+    "url": "/react_address_book/static/css/main.1f793c1a.chunk.css"
+  },
+  {
+    "revision": "f91742afd884d4fa4640",
+    "url": "/react_address_book/static/js/2.16ef68de.chunk.js"
+  },
+  {
+    "revision": "32c97ffce2506f6000c6",
+    "url": "/react_address_book/static/js/main.89878481.chunk.js"
+  },
+  {
+    "revision": "1d8f73e16afb3b0dbda0",
+    "url": "/react_address_book/static/js/runtime-main.b150d28f.js"
+  },
+  {
+    "revision": "b6681619dcc616623083589aa826d2be",
+    "url": "/react_address_book/static/media/default-img.b6681619.png"
+  }
+]);
